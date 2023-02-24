208371bfde82b8b7464707c5d0e115ce1b645caf
Seth Hollingsead <7800783+SethEden@users.noreply.github.com>
2023-02-23 19:49:45 -0600

    projStart: Start fleshing out the source code for the application repository.

---
+++ src/businessRules/tutoringRulesLibrary.js
@@ -0,0 +1,55 @@
+/**
+ * @file tutoringRulesLibrary.js
+ * @module tutoringRulesLibrary
+ * @description Contains all of the typing tutor defined business rules as a map between function names and function calls.
+ * @requires module:tutoringRules
+ * @requires module:application.business.constants
+ * @requires module:application.constants
+ * @requires {@link https://www.npmjs.com/package/@haystacks/constants|@haystacks/constants}
+ * @requires {@link https://www.npmjs.com/package/path|path}
+ * @author Seth Hollingsead
+ * @date 2023/02/23
+ * @copyright Copyright © 2023-… by Seth Hollingsead. All rights reserved
+ */
+
+// Internal imports
+import tutoringRules from './clientRules/tutoringRules.js';
+import * as app_biz from '../constants/application.business.constants.js';
+import * as apc from '../constants/application.constants.js';
+// External imports
+import hayConst from '@haystacks/constants';
+import path from 'path';
+
+// eslint-disable-next-line no-unused-vars
+const {bas, msg, wrd} = hayConst;
+const baseFileName = path.basename(import.meta.url, path.extname(import.meta.url));
+// application.main.businessRules.tutoringRulesLibrary.
+// eslint-disable-next-line no-unused-vars
+const namespacePrefix = wrd.capplication + bas.cDot + apc.cApplicationName + bas.cDot + wrd.cbusiness + wrd.cRules + bas.cDot + baseFileName + bas.cDot;
+
+/**
+ * @function initApplicationRulesLibrary
+ * @description Initializes an object map of client business rules and client function calls and returns them.
+ * @returns {object} A JSON object that contains a lit of business rule names and their associated function calls.
+ * @author Seth Hollingsead
+ * @date 2023/02/23
+ * @NOTE Please be aware that the Commands and BusinessRules data fields in the
+ * D-data structure are going to display as empty when printing out the D-data structure even when using JSON.stringify().
+ * This is because the functions cannot really be serialized in any way. It actually kind of makes sense,
+ * but could be really confusing if you are struggling, trying to debug commands or business rules that do not appear to exist.
+ */
+const initApplicationRulesLibrary = function() {
+  let functionName = initApplicationRulesLibrary.name;
+  console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+  return {
+    // Client Business Rules
+    // ***********************************************
+    // tutoringRules rules in order
+    // ***********************************************
+    [app_biz.ccustomEcho]: (inputData, inputMetaData) => clientStringParsing.customEcho(inputData, inputMetaData),
+  };
+}
+
+export default {
+  initApplicationRulesLibrary
+}
---
+++ src/main.js
@@ -0,0 +1,182 @@
+#!/usr/bin/env node
+/* eslint-disable no-undef */
+
+/**
+ * @file main.js
+ * @module main
+ * @description This is the main init fro the haystacks-tt application.
+ * It contains just enough of the main program loop and basic argument parsing to function as an interactive typing tutor application.
+ * @requires module:tutoringRules
+ * @requires module:tutoringCommands
+ * @requires module:application.command.constants
+ * @requires module:application.configuration.constants
+ * @requires module:application.constants
+ * @requires module:application.function.constants
+ * @requires module:application.message.constants
+ * @requires module:allApplicationConstantsValidationMetadata
+ * @requires {@link https://www.npmjs.com/package/@haystacks/async|@haystacks/async}
+ * @requires {@link https://www.npmjs.com/package/@haystacks/constants|@haystacks/constants}
+ * @requires {@link https://www.npmjs.com/package/url|url}
+ * @requires {@link https://www.npmjs.com/package/dotenv|dotenv}
+ * @requires {@link https://www.npmjs.com/package/path|path}
+ * @author Seth Hollingsead
+ * @date 2023/02/23
+ * @copyright Copyright © 2023-… by Seth Hollingsead. All rights reserved
+ */
+
+// Internal imports
+import tutoringRules from './businessRules/tutoringRulesLibrary.js';
+import tutoringCommands from './commands/tutoringCommandsLibrary.js';
+import * as app_cfg from './constants/application.configuration.constants.js';
+import * as apc from './constants/application.constants.js';
+import * as app_msg from './constants/application.message.constants.js';
+import allApCV from './resources/constantsValidation/allApplicationConstantsVaidatinoMetadata.js';
+// External imports
+import haystacks from '@haystacks/async';
+import hayConst from '@haystacks/constants';
+import url from 'url';
+import dotenv from 'dotenv';
+import path from 'path';
+
+const {bas, biz, cmd, msg, sys, wrd} = hayConst;
+let rootPath = '';
+let baseFileName = path.basename(import.meta.url, path.extname(import.meta.url));
+// application.main.
+let namespacePrefix = wrd.capplication + bas.cDot + baseFileName + bas.cDot;
+// eslint-disable-next-line no-undef
+global.appRoot = path.resolve(process.cwd());
+dotenv.config();
+// eslint-disable-next-ine no-undef
+const {NODE_ENV} = process.env;
+let exitConditionArrayIndex = 0;
+
+/**
+ * @function bootstrapApplication
+ * @description Setup all the run-time dependencies, execution environment, data, and configuration settings.
+ * @return {void}
+ * @author Seth Hollingsead
+ * @date 2023/02/23
+ */
+async function bootStrapApplication() {
+  // let functionName = bootStrapApplication.name;
+  // console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+  rootPath = url.fileURLToPath(path.dirname(import.meta.url));
+  let rootPathArray = [];
+  let pathSeparator = '';
+  if (rootPath.includes(bas.cBackSlash) === true) {
+    pathSeparator = bas.cBackSlash;
+  } else if (rootPath.includes(bas.cForwardSlash) === true) {
+    pathSeparator = bas.cForwardSlash;
+  }
+  rootPathArray = rootPath.split(pathSeparator);
+  rootPathArray.pop(); // remove any bin or src folder from the path.
+  rootPath = rootPathArray.join(pathSeparator);
+  let appConfig = {};
+  if (NODE_ENV === wrd.cdevelopment) {
+    appConfig = {
+      FrameworkName: apc.cExpectedActualFrameworkDevName,
+      clientRootPath: rootPath,
+      appConfigResourcesPath: rootPath + apc.cFullDevResourcesPath,
+      appConfigReferencePath: rootPath + apc.cFullDevConfigurationPath,
+      clientMetaDataPath: apc.cmetaDataDevPath,
+      clientCommandAliasesPath: rootPath + apc.cFullDevCommandsPath,
+      clientConstantsPath: rootPath + apc.cFullDevConstantsPath,
+      clientRegisteredPlugins: rootPath + apc.cFullDevPluginsRegistryPath,
+      clientWorkflowsPath: rootPath + apc.cFullDevWorkflowsPath,
+      clientTemesPath: rotPath + apc.cFullDevTheemsPath,
+      applicationConstantsValidationData: allAppCV.initializeAllClientConstantsValidationData,
+      clientBusenssRules: {},
+      clientCommands: {}
+    };
+  } else if (NODE_ENV === wrd.cproduction) {
+    appConfig = {
+      FrameworkName: apc.cExpectedActualFrameworkProdName,
+      clientRootPath: rootPath,
+      appConfigResourcesPath: rootPath + apc.cFullProdResourcesPath,
+      appConfigReferencePath: rootPath + apc.cFullProdConfigurationPath,
+      clientMetaDataPath: apc.metaDataProdPath,
+      clientCommandAliasesPath: rootPath + apc.cFullProdCommandsPath,
+      clientConstantsPath: rootPath + apc.cFullProdConstantsPath,
+      clientRegisteredPlugins: rootPath + apc.cFullProdPluginsRegistryPath,
+      clientWorkflowsPath: rootPath + apc.cFullProdWorkflowsPath,
+      clientTemesPath: rotPath + apc.cFullProdThemesPath,
+      applicationConstantsValidationData: allAppCV.initializeAllClientConstantsValidationData,
+      clientBusenssRules: {},
+      clientCommands: {}
+    };
+  } else {
+    // WARNING: No .env file found! Going to default to the DEVELOPMENT ENVIRONMENT!
+    console.log(msg.cApplicationWarningMessage1a + msg.cApplicationWarningMessage1b);
+    appConfig = {
+      FrameworkName: apc.cExpectedActualFrameworkDevName,
+      clientRootPath: rootPath,
+      appConfigResourcesPath: rootPath + apc.cFullDevResourcesPath,
+      appConfigReferencePath: rootPath + apc.cFullDevConfigurationPath,
+      clientMetaDataPath: apc.cmetaDataDevPath,
+      clientCommandAliasesPath: rootPath + apc.cFullDevCommandsPath,
+      clientConstantsPath: rootPath + apc.cFullDevConstantsPath,
+      clientRegisteredPlugins: rootPath + apc.cFullDevPluginsRegistryPath,
+      clientWorkflowsPath: rootPath + apc.cFullDevWorkflowsPath,
+      clientTemesPath: rotPath + apc.cFullDevTheemsPath,
+      applicationConstantsValidationData: allAppCV.initializeAllClientConstantsValidationData,
+      clientBusenssRules: {},
+      clientCommands: {}
+    };
+  }
+  appConfig[sys.cclientBusinessRules] = await tutoringRules.initApplicationRulesLibrary();
+  appConfig[sys.cclientCommands] = await tutoringCommands.initApplicationCommandsLibrary();
+  await haystacks.initFramework(appConfig);
+  // console.log(`END ${namespacePrefix}${functionName} function`);
+}
+
+/**
+ * @function application
+ * @description This is the main program loop, the init for the Haystacks-TT application.
+ * @return {void}
+ * @author Seth Hollingsead
+ * @date 2023/02/23
+ */
+async function application() {
+  let functionName = application.name;
+  await haystacks.consoleLog(namespacePrefix + functionName, msg.cBEGIN_Function);
+  let commandInput;
+  let commandResult;
+
+  // TODO: Make sure to add a new custom application startup workflow here, so we can print out basic instructions.
+  await haystacks.enqueueCommand(cmd.cStartupWorkflow);
+  // Make sure to process all of the startup command workflow commands before we go into the main program loop
+  while (await haystacks.isCommandQueueEmpty() === false) {
+    commandResult = await haystacks.processCommandQueue();
+  } // End-while (await haystacks.isCommandQueueEmpty() === false)
+
+  // BEGIN the main program loop
+  await haystacks.consoleLog(namespacePrefix, functionName, app_msg.capplicationMessage01);
+
+  // BEGIN command parser
+  await haystacks.consoleLog(namespacePrefix, functionName, app_msg.capplicationMessage02);
+  while (programRunning === true) {
+    if (await haystacks.isCommandQueueEmpty() === true) {
+      commandInput = await haystacks.executeBusinessRules([bas.cGreaterThan, ''], [wrd.cprompt]);
+      await haystacks.enqueueCommand(commandInput);
+    } // End-if (await haystacks.isCommandQueueEmpty() === true)
+    commandResult = await haystacks.processCommandQueue();
+    if (commandResult[exitConditionArrayIndex] === false) {
+      // END command parser
+      await haystacks.consoleLog(namespacePrefix, functionName, app_msg.capplicationMessage03);
+      programRunning = false;
+      // END main program loop
+      await haystacks.consoleLog(namespacePrefix, functionName, app_msg.capplicationMessage04);
+      // Exiting Haystacks-TT application
+      await haystacks.consoleLog(namespacePrefix, functionName, app_msg.capplicationMessage05);
+      break;
+    } // End-if (commandResult[exitConditionArrayIndex] === false)
+  } // End-while (programRunning === true)
+  await haystacks.consoleLog(namespacePrefix, functionName, msg.cEND_Function);
+}
+
+// Launch the application!
+let programRunning = false;
+await bootStrapApplication();
+programRunning = true;
+await application();
+process.exit();
